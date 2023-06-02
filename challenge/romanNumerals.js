fe71deab5a2e1eaef9e3131b7f3ad5f632370e64
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-02 17:42:16 +0100

    feat: set up my files and folders.

---
+++ challenge/romanNumerals.js
@@ -0,0 +1,5 @@
+const romanNumerals = (num) => {
+
+    // PseudoCode
+
+}
---
+++ test/romanNumerals.test.js
@@ -0,0 +1,35 @@
+import { romanNumerals } from '../challenge/romanNumerals';
+
+describe('romanNumerals', () => {
+    test('return any given input of number to roman numbers', () => {
+        // Arrange
+        const expected = 'II';
+
+        // Act
+        const result = romanNumerals(2)
+
+        // Assert
+        expect(result).toBe(expected)
+    })
+})
+
+
+
+
+
+
+
+
+// import { sumMultiples, isValidDNA, getComplementaryDNA, isItPrime, createMatrix, areWeCovered } from '../challenges/exercise006';
+
+// describe('sumMultiples', () => {
+// 	test('returns the sum of any numbers which are a multiple of 3 or 5', () => {
+// 		// Arrange
+// 		const expected = 8;
+
+// 		// Act
+// 		const result = sumMultiples([1, 3, 5]);
+
+// 		// Assert
+// 		expect(result).toBe(expected);
+// 	});

730940bab33a69beea86a737a563582e71e5ef5d
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-02 22:51:15 +0100

    feat: Implemented the kata and passing all tests

--- challenge/romanNumerals.js
+++ challenge/romanNumerals.js
@@ -1,7 +1,34 @@
 export const romanNumerals = (num) => {
     if (num === undefined) throw new Error('number is required'); 
 
-    
+    const roman = {
+            M: 1000,
+            CM: 900,
+            D: 500,
+            CD: 400,
+            C: 100,
+            XC: 90,
+            L: 50,
+            XL: 40,
+            X: 10,
+            IX: 9,
+            V: 5,
+            IV: 4,
+            I: 1    
+    };
+
+    let accumulator = '';
+
+    for(const key in roman) {
+        const numberValue = roman[key];
+
+        while(numberValue <= num) {
+            num -= numberValue;
+            accumulator += key;
+        }
+    }
+
+    return accumulator;
 
 };
 
